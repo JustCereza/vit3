eba030bb93aca76b043d55bfc1bf458b200b07a7
JustCereza <josevsl656@gmail.com>
2024-09-05 19:18:58 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
   server: {
     host: true,
-    port: 80
+    port: 80,
   },
 });
